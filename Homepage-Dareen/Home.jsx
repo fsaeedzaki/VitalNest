da2d4efadcf04861e87e7b4c3b176370b84e7c4e
Dodogame3sl <dareendodo899@gmail.com>
2025-09-19 04:58:09 +0300

    the home page

---
+++ Homepage-Dareen/Home.jsx
@@ -0,0 +1,9 @@
+import Homepage from "../Component/Homepage"
+function Home({}){
+    return(
+        <div>
+            <Homepage image="./src/assets/doctors.png" i="./src/assets/heart.jpg" p="./src/assets/yd.jpg"/>
+        </div>
+    )
+}
+export default Home;
